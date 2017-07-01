0f118bc1c4070c3f52ade92f478fad507aeca737
langshiquan <langshiquan@baidu.com>
2017-07-01 19:31:01 +0800

    firstone

---
+++ src/com/baidu/annotation/Main.java
@@ -0,0 +1,10 @@
+package com.baidu.annotation;
+
+/**
+ * Created by langshiquan on 17/7/1.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello,world!");
+    }
+}
--- src/com/baidu/reflection/Main.java
+++ src/com/baidu/reflection/Main.java
@@ -1,3 +1,5 @@
+package com.baidu.reflection;
+
 public class Main {
 
     public static void main(String[] args) {
